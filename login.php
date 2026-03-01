2bc66b7381d0a4c9625d87ad657a3cb0c095edac
RIOABLAS <r.ablas.514023@umindanao.edu.ph>
2026-03-01 11:42:07 +0800

    New file created for registration page. Login page is renamed to login.php.

--- login.php
+++ login.php
@@ -50,12 +50,12 @@
 					
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn">
-							Login
+							Sign In
 						</button>
 					</div>
 
 					<div class="text-center p-t-12">
-						<a class="txt2" href="#">
+						<a class="txt2" href="register.php">
 							Don't have an Account?
 						</a>
 					</div>
